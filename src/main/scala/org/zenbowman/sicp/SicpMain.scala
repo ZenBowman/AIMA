aca3be86008b09a117835b845083918b4a17bbf7
Prasan Samtani <PrasanSamtani@gmail.com>
2014-06-21 14:40:53 -0700

    Exercise 2.1 of SICP done

--- src/main/scala/org/zenbowman/sicp/SicpMain.scala
+++ src/main/scala/org/zenbowman/sicp/SicpMain.scala
@@ -1,7 +1,12 @@
-package org.zenbowman.sicp;
+package org.zenbowman.sicp
 
 object SicpMain {
   def main(args: Array[String]) {
-    println("Hello")
+    args(0) match {
+      case "2" =>
+        Chapter2(args(1))
+      case _ =>
+        println("Unknown chapter")
+    }
   }
 }
