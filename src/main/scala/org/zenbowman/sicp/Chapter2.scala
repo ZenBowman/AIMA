464cf3b6f5b62329f45b0a30d0f40bfde178f4e6
Prasan Samtani <PrasanSamtani@gmail.com>
2014-06-22 10:40:03 -0700

    Finished 2.7 and 2.8

--- src/main/scala/org/zenbowman/sicp/Chapter2.scala
+++ src/main/scala/org/zenbowman/sicp/Chapter2.scala
@@ -14,6 +14,8 @@ object Chapter2 {
     exercise match {
       case "1" =>
         Exercise1()
+      case "8" =>
+        ExtendedExercise.exercise8()
       case _ =>
         println("Unknown exercise")
     }
@@ -49,4 +51,45 @@ object Chapter2 {
       printRat(5, -7)
     }
   }
+
+  object ExtendedExercise {
+    case class Interval(lowerBound: Double, upperBound: Double) {
+      override def toString() = s"$lowerBound:$upperBound"
+    }
+    def lowerBound(i: Interval) = i.lowerBound
+    def upperBound(i: Interval) = i.upperBound
+
+    def makeInterval(a: Double, b: Double) = {
+      Interval(a, b)
+    }
+
+    def subInterval(a: Interval, b: Interval) = {
+      Interval(
+        lowerBound(a) - upperBound(b),
+        upperBound(a) - lowerBound(b)
+      )
+    }
+
+    implicit def PairToInterval(pp : (Double, Double)) = {
+      Interval(pp._1, pp._2)
+    }
+
+    def printBinaryFunction(a: Interval, b: Interval, 
+      f:(Interval, Interval) => Interval, fname: String) {
+      println(s"($fname $a $b) = ${f(a, b)}")
+    }
+
+    def printSub(a: Interval, b: Interval) {
+      printBinaryFunction(a, b, subInterval, "sub-interval")
+    }
+
+    def exercise8() {
+      printSub(Interval(3, 5), Interval(1, 1))
+      printSub(Interval(3, 3), Interval(1, 4))
+      printSub(Interval(5.0, 7.0), Interval(-5.5, 20.5))
+    }
+
+  }
+
+
 }
